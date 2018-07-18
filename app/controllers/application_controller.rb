3142b625b9202c63a13ac506442c77f54520c32a
Peter Kim <peterckim96@gmail.com>
2018-07-18 19:55:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     end
     
     post '/teams' do
-      @team = params[:team]
+      @team = Team.new(params[:team])
       
       params[:team][:members].each do |details|
         Hero.new(details)
