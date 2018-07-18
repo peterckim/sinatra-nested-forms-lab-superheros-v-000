9ea64d2e4f53bd6b840ee50f4867e8f340fe3956
Peter Kim <peterckim96@gmail.com>
2018-07-18 19:58:40 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,15 @@
+class Team
+  attr_accessor :name, :motto
+  
+  TEAM = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    TEAM << self
+  end
+  
+  def self.all
+    TEAM
+  end
+end
